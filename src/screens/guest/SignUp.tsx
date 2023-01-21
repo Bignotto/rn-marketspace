eeafe1347817933c129ed0f603b5f4222cb273fd
Thiago Bignotto <bignotto@gmail.com>
2023-01-21 06:47:59 -0300

    added and configure react stack navigation and implemented signup screen to validate navigation

--- src/screens/guest/SignUp.tsx
+++ src/screens/guest/SignUp.tsx
@@ -1,6 +1,6 @@
 import { TextInput } from "@components/TextInput";
 import {
-  Avatar,
+  Box,
   Center,
   Heading,
   Image,
@@ -11,62 +11,80 @@ import {
 
 import LogoPng from "@assets/LogoSm.png";
 import { GenericButton } from "@components/GenericButton";
-import { User } from "phosphor-react-native";
+import { useNavigation } from "@react-navigation/native";
+import { PencilSimpleLine, User } from "phosphor-react-native";
 import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export function SignUp() {
+  const navigation = useNavigation();
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
       bgColor="gray.100"
     >
-      <VStack
-        flex={1}
-        px={10}
-        pb={16}
-        borderBottomRadius={28}
-        bgColor="gray.200"
-      >
+      <VStack flex={1} px={10} bgColor="gray.200">
         <Center mt={getStatusBarHeight() + 40}>
           <Image source={LogoPng} alt="folder inside globe" />
         </Center>
-        <Center mt="10">
+        <Center mt="4">
           <Heading>Boas Vindas!</Heading>
           <Text fontSize="xl" textAlign="center">
             Crie sua conta e useo espaço para comprar itens variados e vender
             seus produtos
           </Text>
         </Center>
-        <Center>
-          <Avatar
-            bg="blue.50"
-            alignSelf="center"
-            size="2xl"
-            source={{
-              uri: "http://localhost:3333",
-            }}
+        <Center mt="8">
+          <Box
+            borderWidth={3}
+            borderColor="#647AC7"
+            w={88}
+            h={88}
+            borderRadius="full"
           >
-            <User size={24} />
-          </Avatar>
+            <Box
+              position="absolute"
+              left={16}
+              top={12}
+              bgColor="#647AC7"
+              w="10"
+              h="10"
+              borderRadius="full"
+            >
+              <Center flex={1}>
+                <PencilSimpleLine color="#FFFFFF" />
+              </Center>
+            </Box>
+            <Center flex={1}>
+              <User size={54} color="#9F9BA1" />
+            </Center>
+          </Box>
         </Center>
         <Center mt="4">
+          <TextInput placeholder="Nome" mb="4" />
           <TextInput
             placeholder="E-mail"
             keyboardType="email-address"
             autoCapitalize="none"
             mb="4"
           />
+          <TextInput placeholder="Telefone" mb="4" />
           <TextInput placeholder="Senha" secureTextEntry mb="4" />
-          <GenericButton title="Entrar" />
+          <TextInput placeholder="Confirmar senha" secureTextEntry mb="4" />
+          <GenericButton title="Criar" variant="dark" />
+        </Center>
+        <Center pt="16" mb="20">
+          <Heading fontSize="xl" mb="4">
+            Já tem uma conta?
+          </Heading>
+          <GenericButton
+            title="Voltar para o login"
+            variant="light"
+            onPress={() => navigation.goBack()}
+          />
         </Center>
       </VStack>
-      <Center bgColor="gray.100" px={10} pt="16" mb="20">
-        <Heading fontSize="xl" mb="4">
-          Ainda não tem acesso?
-        </Heading>
-        <GenericButton title="Criar uma nova conta" variant="light" />
-      </Center>
     </ScrollView>
   );
 }
