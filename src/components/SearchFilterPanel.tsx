8eb8fa30df224d9980985c5edd3a98c4d82b54c8
Thiago Bignotto <bignotto@gmail.com>
2023-01-26 06:15:47 -0300

    save commit

---
+++ src/components/SearchFilterPanel.tsx
@@ -0,0 +1,48 @@
+import {
+  Box,
+  Checkbox,
+  HStack,
+  ScrollView,
+  Switch,
+  Text,
+  useTheme,
+  VStack,
+} from "native-base";
+import { useState } from "react";
+import { GenericButton } from "./GenericButton";
+
+export function SearchFilterPanel() {
+  const theme = useTheme();
+
+  const [acceptTrade, setAcceptTrade] = useState(false);
+  return (
+    <VStack px={10} flex={1}>
+      <ScrollView>
+        <VStack flex={1} alignItems="flex-start">
+          <Text>Condição</Text>
+          <HStack>
+            <Text>USADO</Text>
+            <Text>NOVO</Text>
+          </HStack>
+          <Text>Aceita troca?</Text>
+          <Switch size="md" onTrackColor={theme.colors.blue[400]} />
+          <VStack>
+            <Text>Meios de pagamento aceitos</Text>
+            <Checkbox value="dinheiro">Dinheiro</Checkbox>
+            <Checkbox value="pix">Pix</Checkbox>
+            <Checkbox value="cc">Cartão de Crédito</Checkbox>
+            <Checkbox value="deposito">Depósito Bancário</Checkbox>
+          </VStack>
+        </VStack>
+      </ScrollView>
+      <HStack justifyContent="space-between" mb="3" bgColor="pink.400">
+        <Box w="48%" mr="2">
+          <GenericButton title="Resetar filtros" variant="light" />
+        </Box>
+        <Box w="48%" ml="2">
+          <GenericButton title="Aplicar filtros" variant="dark" />
+        </Box>
+      </HStack>
+    </VStack>
+  );
+}
