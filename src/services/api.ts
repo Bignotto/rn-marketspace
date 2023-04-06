7f699826e23ca7455346717b86d1c4c835cf1b8f
Thiago Bignotto <bignotto@gmail.com>
2023-04-06 07:27:04 -0300

    implemeted request queue

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
+import { storageAuthTokenGet } from "@storage/storageAuth";
 import { AppError } from "@utils/AppError";
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 
 type SignOut = () => void;
 
@@ -7,19 +8,59 @@ type ApiInstanceProps = AxiosInstance & {
   registerInterceptorTokenManager: (signOut: SignOut) => () => void;
 };
 
+type PromiseType = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+};
+
 const api = axios.create({
   baseURL: process.env.APP_API_URL,
 }) as ApiInstanceProps;
 
+let requestQueue: Array<PromiseType> = [];
+let isRefreshing = false;
+
 api.registerInterceptorTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
     (response) => response,
-    (error) => {
-      if (error.response && error.response.data) {
-        return Promise.reject(new AppError(error.response.data.message));
+    async (requestError) => {
+      if (requestError?.response?.status === 401) {
+        if (
+          requestError.response.data?.message === "token.expired" ||
+          requestError.response.data?.message === "token.invalid"
+        ) {
+          const { refresh_token } = await storageAuthTokenGet();
+          if (!refresh_token) {
+            signOut();
+            return Promise.reject(requestError);
+          }
+
+          const originalRequest = requestError.config;
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              requestQueue.push({
+                onSuccess: (token: string) => {
+                  originalRequest.headers = {
+                    Authorization: `Bearer ${token}`,
+                  };
+                },
+                onFailure: (error: AxiosError) => {
+                  reject(error);
+                },
+              });
+            });
+          }
+          isRefreshing = true;
+          //NEXT: process request queue
+        }
+        signOut();
       }
 
-      return Promise.reject(error);
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message));
+      }
+
+      return Promise.reject(requestError);
     }
   );
 
